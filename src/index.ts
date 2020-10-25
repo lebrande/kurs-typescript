e365e52d118fdf5d1f49d0f0ac489f869847f9a9
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-10-25 21:09:58 +0100

    Zmienne.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Witaj w kursie TypeSctipt na JS Dżem!');
+const name1: string = 'Kuba';
+const name3: string = `Kuba ${name1}`;
+
+const price = 55;
+
+// niedozwolone nadpisywanie stałych const
+// name1 = 'takie coś innego';
+// price = 44;
