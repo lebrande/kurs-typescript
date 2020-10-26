5bc815e4ec34ef61280f4633e319dd3f107a337a
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-10-26 09:45:47 +0100

    Tablice.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const name1: string = 'Kuba';
-const name3: string = `Kuba ${name1}`;
+const points: number[] = [49, 48, 25];
+const cities: Array<string | number> = ['London', 'Zurich', 'New York', 55];
 
-const price = 55;
+const emptyArray = [];
 
-// niedozwolone nadpisywanie stałych const
-// name1 = 'takie coś innego';
-// price = 44;
+type User = [
+  name: string, 
+  points: number, 
+  status: string
+];
+const user: User = ['Kuba', 49, 'active'];
+
+const point4 = points[3];
+const [, ...cities2] = cities;
+const [name1, points1, status1] = user;
+const someData = emptyArray[100];
+
+const things = [...cities, 66, 0];
+
+console.log(things);
